0bfcf4e8b5f332c57d025dbbea0a39c152b031fc
JoeyLearnsToCode <zilin.wang.1024@gmail.com>
2026-04-02 14:14:39 +0800

    feat(relay): 处理特定渠道模型名称后缀
    - 如果模型名称以 "_channel${id}" 结尾，则去掉该后缀

--- relay/helper/model_mapped.go
+++ relay/helper/model_mapped.go
@@ -4,13 +4,24 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"strings"
 
+	"github.com/QuantumNous/new-api/constant"
 	"github.com/QuantumNous/new-api/dto"
 	"github.com/QuantumNous/new-api/relay/common"
 	"github.com/gin-gonic/gin"
 )
 
 func ModelMappedHelper(c *gin.Context, info *common.RelayInfo, request dto.Request) error {
+	// 如果存在 specific_channel_id 且模型名称以 _channel${id} 结尾，则去掉该后缀
+	specificChannelId := c.GetString(string(constant.ContextKeyTokenSpecificChannelId))
+	if specificChannelId != "" {
+		suffix := "_channel" + specificChannelId
+		if strings.HasSuffix(info.OriginModelName, suffix) {
+			info.OriginModelName = strings.TrimSuffix(info.OriginModelName, suffix)
+		}
+	}
+
 	// map model name
 	modelMapping := c.GetString("model_mapping")
 	if modelMapping != "" && modelMapping != "{}" {
