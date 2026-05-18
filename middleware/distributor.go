9cb7281d38226d38b97cc5bada6e582674821809
JoeyLearnsToCode <zilin.wang.1024@gmail.com>
2026-05-18 17:09:26 +0800

    perf: 优化模型名称处理逻辑
    - 扩展了 specific_channel_id 处理逻辑，支持更多字符串格式

--- middleware/distributor.go
+++ middleware/distributor.go
@@ -56,12 +56,18 @@ func Distribute() func(c *gin.Context) {
 				return
 			}
 
-			// 如果存在 specific_channel_id 且模型名称以 _channel${id} 结尾，则去掉该后缀
+			// 如果存在 specific_channel_id 且模型名称包含特定字符串，则去掉
 			if channelId != "" {
-				suffix := "_channel" + strconv.Itoa(id)
-				modelRequest.Model = strings.TrimSuffix(modelRequest.Model, suffix)
+				idStr := strconv.Itoa(id)
+				r := strings.NewReplacer(
+					"_channel"+idStr+"_", "",
+					"_channel"+idStr, "",
+					"_channel-"+idStr+"_", "",
+					"_channel-"+idStr, "",
+				)
+				modelRequest.Model = r.Replace(modelRequest.Model)
 			}
-			
+
 			// 应用全局模型映射
 			targetModels, usingGlobalModelMapping := model_setting.ResolveGlobalModelMappings(modelRequest.Model)
 			if usingGlobalModelMapping {
